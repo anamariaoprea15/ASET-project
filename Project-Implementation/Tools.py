ba560e87ab916f2bc2192ec7483588de8388e69a
k-rot <129273719+k-rot@users.noreply.github.com>
2023-12-05 04:02:23 +0200

    Added MOP

--- Project-Implementation/Tools.py
+++ Project-Implementation/Tools.py
@@ -1,35 +1,77 @@
-import random
 import numpy as np
+from functools import wraps
+import logging
+import time
+from pprint import pprint
 
-def log_syndrome_update(func):
+# Configure the logging system
+def configure_logging():
+    if not logging.getLogger().handlers:
+        logging.basicConfig(filename='monitoring.log', level=logging.DEBUG)
+
+configure_logging()
+
+def log_execution_time(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
+        start_time = time.time()
         result = func(*args, **kwargs)
-        args[0].notify_observers()  # Notify observers after the update
+        execution_time = time.time() - start_time
+        logging.info(f"{func.__name__} executed in {execution_time:.4f} seconds")
         return result
 
     return wrapper
 
-def log_random_permutation(func):
+def log_random_permutation(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
-        args[0].notify_observers()  # Notify observers after generating a random permutation
+        logging.info("Random permutation generated.")
+        return result
+
+    return wrapper
+
+def log_syndrome_update(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        old_syndrome = args[0].syndrome  # Get the current syndrome before the update
+        result = func(*args, **kwargs)
+        new_syndrome = args[0].syndrome  # Get the updated syndrome
+
+        # Check if the syndrome has changed before logging
+        if old_syndrome != new_syndrome:
+            args[0].notify_observers()  # Notify observers after the update
+            logging.info(f"Syndrome has been updated: {new_syndrome}")
         return result
 
     return wrapper
 
 class Tools:
     def __init__(self, k, n, t, syndrome):
+        self.k = k
+        self.n = n
         self.r = n - k
         self.H = np.empty((self.r, n))
         self.t = t
         self._syndrome = syndrome
         self._observers = []
-    
+        logging.info(f"Initial syndrome: {self._syndrome}") # Log the initial syndrome
+
+    @property
+    def state(self):
+        return {
+            "k": self.k,
+            "n": self.n,
+            "r": self.r,
+            "t": self.t,
+            "syndrome": self.syndrome,
+            #"syndrome": np.array(self.syndrome).tolist(),  # Convert to list if self.syndrome is a NumPy array
+        }
+
     def get_parameters(self):
         return self.k, self.n, self.r, self.t, self.syndrome
 
     @property
-    @log_syndrome_update
     def syndrome(self):
         return self._syndrome
 
@@ -38,7 +80,9 @@ class Tools:
     def syndrome(self, new_syndrome):
         if new_syndrome != self._syndrome:
             self._syndrome = new_syndrome
+            self.notify_observers()
 
+    @log_execution_time
     @log_random_permutation
     def generate_random_permutation(self, n):
         """
@@ -54,24 +98,34 @@ class Tools:
             on the line will tell the new position of said column. The matrix will be multiplied
             from the right. (Ex: H x P)
         """
-        result = np.zeros([n, n], dtype=int)
-        positions = [i for i in range(n)]  # [0, 1, 2, ..., n-1]
-        random.shuffle(positions)
+        try:
+            #result = 1 / 0 # Simulate an error by dividing by zero
+            old_syndrome = self.syndrome.copy()  # Save the current syndrome before the update
 
-        for i in positions:
-            result[i][positions[i]] = 1
-        print(result)
-        return result
+            positions = np.arange(n)
+            np.random.shuffle(positions)
 
-    def weight(self, e): # tested
+            result = np.eye(n, dtype=int)[positions]
+            print("Random permutation generated.")  # Just for testing 
+            print(result)
+
+         # Check if the syndrome has changed before notifying observers
+            if old_syndrome != self.syndrome:
+                self.notify_observers()
+
+            return result
+        
+        except ValueError as ve:
+            logging.error(f"ValueError in generating random permutation: {ve}")
+            raise
+        except Exception as e:
+            logging.exception("Error in generating random permutation")
+
+    def weight(self, e):
         """
         Returns the weight of the error vector
         """
-        w = 0
-        for digit in e:
-            if digit != 0:
-                w += 1
-        return w
+        return np.linalg.norm(e, ord=0)
 
     def reduced_row_echelon_form(self, H):
         """
@@ -87,8 +141,8 @@ class Tools:
         Every change that gets applied to H will be applied to P respectively, so as to obtain
         the transformation matrix P needed.
         """
-        lead = 0 # initial lead position
-        rows, cols = H.shape # dimensions of H
+        lead = 0  # initial lead position
+        rows, cols = H.shape  # dimensions of H
         P = np.identity(cols)  # Initialize P as the identity matrix
 
         for r in range(rows):
@@ -124,8 +178,7 @@ class Tools:
 
         return H, P
 
-
-    def subscribe(self, observer): 
+    def subscribe(self, observer):
         self._observers.append(observer)
 
     def unsubscribe(self, observer):
@@ -140,6 +193,8 @@ class SyndromeObserver:
         # Handle the updated syndrome
         print("Syndrome has been updated:", tools.syndrome)
 
+
+
 # Usage example:
 if __name__ == "__main__":
     syndrome_observer = SyndromeObserver()
@@ -152,3 +207,6 @@ if __name__ == "__main__":
 
     # Simulate generating a random permutation
     tools.generate_random_permutation(4)
+
+    state_info = tools.state
+    pprint(state_info)
